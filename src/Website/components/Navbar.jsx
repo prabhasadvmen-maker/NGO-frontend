5c698210fa8d6d0ed60d87cd47275092a461a324
Brahmakosh Team <brahmakoshtech@gmail.com>
2026-07-27 16:31:41 +0530

    first commit

--- src/Website/components/Navbar.jsx
+++ src/Website/components/Navbar.jsx
@@ -74,9 +74,9 @@ export const Navbar = () => {
                 <Phone size={11} className="text-white" />
                 <span>+91 88600 36008</span>
               </a>
-              <a href="mailto:Support.savitramfoundation@gmail.com" className="flex items-center gap-1.5 hover:text-green-400 transition-colors">
+              <a href="mailto:info@savitramfoundation.org" className="flex items-center gap-1.5 hover:text-green-400 transition-colors">
                 <Mail size={11} className="text-white" />
-                <span>Support.savitramfoundation@gmail.com</span>
+                <span>info@savitramfoundation.org</span>
               </a>
             </div>
 
